d68ff816ec355e9d31bce65ee5c0e17432ef4788
Newton Ombese <newtonombese1@gmail.com>
2023-07-08 07:55:52 +0300

    :art: Add solution to the lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Mohammad Newton"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}`
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+console.log(shortGreeting)
